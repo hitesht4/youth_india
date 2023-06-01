7f2c226a4f769f165aca9bd0c39b0697d41d25d7
hitesht <hiteshtarwani4@gmail.com>
2023-06-01 12:22:25 +0530

    check

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const connect = require("./src/configs/db");
 const booksRouter = require("./src/controllers/books.controller");
 const app = express();
 const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Books Library");
+});
 app.use(cors());
 app.use(express.json());
 app.use("/books", booksRouter);
